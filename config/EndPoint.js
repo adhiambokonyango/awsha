740ba8615acabe762056eb3dd3da2efaa8eb86c8
adhiambokonyango <marykonyango74@gmail.com>
2021-04-02 08:53:54 +0300

    tokens

--- config/EndPoint.js
+++ config/EndPoint.js
@@ -2,4 +2,4 @@
 // export const transactionsIp = "http://transaction.waondo.agweria.com";
 
  const ip = "http://127.0.0.1:8080";
-
+module.exports = ip;
--- services/api_connector/ApiConnector.js
+++ services/api_connector/ApiConnector.js
@@ -1,9 +1,9 @@
 const axios =require( "axios");
 
 const querystring =require( "querystring");
-//const EndPoint =require( "../../config/EndPoint");
+let ip =require( "../../config/EndPoint");
 //const { header } = require("request");
-const ip = "http://127.0.0.1:8080";
+//const ip = "http://127.0.0.1:8080";
 
 const token = '123'
 
